df2203d63af9c8e18eab014ed24704da0fa9946e
kevinefraim <kevinefraim02@gmail.com>
2023-01-26 15:18:11 -0300

    fix nextjs config

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    unoptimized:true
+  },
+  exportPathMap: ()=>  {return {'/': { page: '/' }}}
 }
 
 module.exports = nextConfig
